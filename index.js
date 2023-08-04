4d8c165c3731bf87a98aaf41715b4809a172c6cf
Jorge-QA <jorge.q.a@hotmail.com>
2023-08-03 23:21:23 -0600

    lista auth por correo...

--- index.js
+++ index.js
@@ -28,19 +28,23 @@ app.use(cors({
 const authRoutes = require("./controllers/auth");
 const validateToken = require("./controllers/validateToken");
 const admin = require("./controllers/admin");
-const prompts =require("./controllers/prompt")
-const openAi =require("./controllers/openAiController")
+const prompts =require("./controllers/prompt");
+const openAi =require("./controllers/openAiController");
+const autentication =require("./controllers/admin");
+
 
 // route middlewares para validaciónes
 //ruta de validacciones de login y registro de usuario
 app.use('/api/session',authRoutes)
 // se ejecuta la validación antes de pasar a las rutas...
 ////ruta de obtención y mandejo de información de usuarios
-app.use('/api/admin', admin) // Reactivar... probando autenticación...
+app.use('/api/admin',validateToken, admin) 
 ////ruta de obtención y mandejo de información de prompts
 app.use('/api/handle',validateToken, prompts)
 ////ruta de api OpenAi
 app.use('/api/openAi',validateToken, openAi)
+//para la autenticación por correo
+app.use('/api/email',autentication) 
 
 
 // iniciar server

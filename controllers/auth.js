2726a671013c3640294621dff21eed0d2642a0ae
Jorge-QA <jorge.q.a@hotmail.com>
2023-08-07 03:13:44 -0600

    algo pasa con el sms

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,8 +4,10 @@ sgMail.setApiKey(process.env.TWILIO_KEY);
 //para el uso de envío de mensajes
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
-const verifySid = process.env.TWILIO_VERIFY_SID;
+const twilioPhone = process.env.TWILIO_PHONE_NUMBER;
+const miPhone = process.env.MI_PHONE;
+const client = require("twilio")(accountSid, authToken);
+//const verifySid = process.env.TWILIO_VERIFY_SID;
 
 //Para hacer nuestra ruta en un archivo separado
 const router = require("express").Router();
@@ -56,6 +58,16 @@ router.post("/login", async (req, res) => {
       .status(400)
       .json({ error: true, mensaje: "Valide sus datos de inicio de sesión" });
 
+  // Genera un número aleatorio entre 0 y 9999
+  const randomNumber = Math.floor(Math.random() * 10000);
+  const formattedNumber = String(randomNumber).padStart(4, "0");
+
+  //podemos enviar el sms si se tiene activo el tfa
+  if (user.tfa) {
+    //sendMessage(user.first_name, formattedNumber);
+    console.log("Esta activa la tfa");
+  }
+
   //acá creamos el token
   const token = jwt.sign(
     {
@@ -64,6 +76,7 @@ router.post("/login", async (req, res) => {
       rol: user.rol,
       state: user.state,
       tfa: user.tfa,
+      code: formattedNumber,
     },
     process.env.TOKEN_SECRET
   );
@@ -78,13 +91,11 @@ router.post("/register", async (req, res) => {
   //validaciones de usuario(esquema)
   const { error } = schemaRegister.validate(req.body); //
   if (error) {
-    return res
-      .status(400)
-      .json({
-        error: true,
-        mensaje:
-          "Valide los datos ingresados mayores a 4 caracteres y formato de correo válido",
-      });
+    return res.status(400).json({
+      error: true,
+      mensaje:
+        "Valide los datos ingresados mayores a 4 caracteres y formato de correo válido",
+    });
   }
 
   //valida si el correo ya está registrado
@@ -123,23 +134,18 @@ router.post("/register", async (req, res) => {
   }
 });
 
-
-
 // Configuración del envío del correo
 
-function enviarCorreoAuth(
-  destinatario,
-  nombreUsuario,
-  id
-) {
+function enviarCorreoAuth(destinatario, nombreUsuario, id) {
   // Contenido del correo
   const asunto = "Autenticación de usuario APP Open AI";
   const mensaje =
-    `Hola ${nombreUsuario},\n\n` + "Gracias por registrarte, solo falta un paso más, ingresa al link para autentificar tu correo\n\n" +
+    `Hola ${nombreUsuario},\n\n` +
+    "Gracias por registrarte, solo falta un paso más, ingresa al link para autentificar tu correo\n\n" +
     `Tu link de autenticación es: http://127.0.0.1:5500/client/autenticacion.html?id=${id}\n\n` +
     "¡Gracias por utilizar nuestra app!";
 
-    console.log('http://127.0.0.1:5500/client/autenticacion.html?id=' + id)
+  console.log("http://127.0.0.1:5500/client/autenticacion.html?id=" + id);
 
   const correo = {
     to: destinatario,
@@ -152,40 +158,32 @@ function enviarCorreoAuth(
   };
 
   // Enviar el correo
-  // sgMail
-  //   .send(correo)
-  //   .then(() => {
-  //     console.log("Correo enviado con éxito");
-  //   })
-  //   .catch((error) => {
-  //     console.error("Error al enviar el correo:", error);
-  //   });
+  sgMail
+    .send(correo)
+    .then(() => {
+      console.log("Correo enviado con éxito");
+    })
+    .catch((error) => {
+      console.error("Error al enviar el correo:", error);
+    });
 }
 
+//Función para envío de sms
+function sendMessage(name, code) {
+  client.messages
+    .create({
+      body: `Hola ${name} tu código es el:  ${code}`,
+      from: twilioPhone,
+      to: miPhone,
+    })
+    .then((message) => console.log(message.sid))
+    .catch((error) => console.error(error));
+}
 
-// Download the helper library from https://www.twilio.com/docs/node/install
-// Set environment variables for your credentials
-// Read more at http://twil.io/secure
-
-//ejemplo sms y validación
-
-// client.verify.v2
-//   .services(verifySid)
-//   .verifications.create({ to: "+50683151689", channel: "sms" })
-//   .then((verification) => console.log(verification.status))
-//   .then(() => {
-//     const readline = require("readline").createInterface({
-//       input: process.stdin,
-//       output: process.stdout,
-//     });
-//     readline.question("Please enter the OTP:", (otpCode) => {
-//       client.verify.v2
-//         .services(verifySid)
-//         .verificationChecks.create({ to: "+50683151689", code: otpCode })
-//         .then((verification_check) => console.log(verification_check.status))
-//         .then(() => readline.close());
-//     });
-//   });
+//'prueba:
+const name = "Jorginho"
+const code = "1225"
+sendMessage(name, code)
 
 //Sirve para hacer archivos aparte
 module.exports = router;

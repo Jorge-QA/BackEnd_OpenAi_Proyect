128045bc9104b04f864bb0dd4842aa3275129374
Jorge-QA <jorge.q.a@hotmail.com>
2023-08-16 00:28:21 -0600

    detalles: correo/sms

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv'); // Agrega esta línea para cargar las variables de entorno
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv"); // Agrega esta línea para cargar las variables de entorno
 dotenv.config(); // Carga las variables de entorno del archivo .env
 
 //para utilizar GraphQl
-const {graphqlHTTP} = require("express-graphql"); // le puse llaves para que funcionara
+const { graphqlHTTP } = require("express-graphql"); // le puse llaves para que funcionara
 const { graphQLschema } = require("./graphql-schema.js");
 //const { graphQLschema } = require("../GraphQL/graphql-schema"); // no me está funcionando la ruta en la carpeta GraphQL
 
 // database connection
 const db = mongoose.connect(process.env.DB_Connection, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
-const bodyparser = require('body-parser');
-require('dotenv').config()// para las variables de entorno...
+const bodyparser = require("body-parser");
+require("dotenv").config(); // para las variables de entorno...
 
 const app = express();
 
@@ -24,35 +24,41 @@ app.use(bodyparser.json());
 
 // check for cors
 const cors = require("cors");
-app.use(cors({
-  domains: '*',
-  methods: "*"
-}));
+app.use(
+  cors({
+    domains: "*",
+    methods: "*",
+  })
+);
 
 // importar rutas
 const authRoutes = require("./controllers/auth");
 const validateToken = require("./controllers/validateToken");
 const admin = require("./controllers/admin");
-const prompts =require("./controllers/prompt");
-const openAi =require("./controllers/openAiController");
-const autentication =require("./controllers/auth");
-
+const prompts = require("./controllers/prompt");
+const openAi = require("./controllers/openAiController");
+const autentication = require("./controllers/auth");
 
 // route middlewares para validaciónes
 //ruta de validacciones de login y registro de usuario
-app.use('/api/session',authRoutes)
+app.use("/api/session", authRoutes);
 // se ejecuta la validación antes de pasar a las rutas...
 ////ruta de obtención y mandejo de información de usuarios
-app.use('/api/admin',validateToken, admin) 
+app.use("/api/admin", validateToken, admin);
 ////ruta de obtención y mandejo de información de prompts
-app.use('/api/handle',validateToken, prompts)
+app.use("/api/handle", validateToken, prompts);
 ////ruta de api OpenAi
-app.use('/api/openAi',validateToken, openAi)
+app.use("/api/openAi", validateToken, openAi);
 //para la autenticación por correo
-app.use('/api/email',autentication) 
-app.use('/api/email', admin) 
+app.use("/api/email", autentication);
+app.use("/api/email", admin);
 
-const {promptGet, promptsByName, promptsByTags, userPrompt} = require("./controllers/promptsController.js");
+const {
+  promptGet,
+  promptsByName,
+  promptsByTags,
+  userPrompt,
+} = require("./controllers/promptsController.js");
 //const {usersGet} = require("./controllers/usersController.js");
 
 // expose in the root element the different entry points of the
@@ -77,4 +83,4 @@ app.use(
 // http://localhost:3001/graphql    para abrir en navegador GraphiQl
 
 // iniciar server
-app.listen(3001, () => console.log(`Servidor corriendo en puerto: 3001!`))
+app.listen(3001, () => console.log(`Servidor corriendo en puerto: 3001!`));
